b320e8796d3f12416d7a7742c492530f3eb9b1af
tanahernandez28 <110550156+tanahernandez28@users.noreply.github.com>
2022-10-16 21:34:57 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function ingresar() {
+    const email = document.getElementById('email').value;
+    const contra = document.getElementById('contraseña').value;
+    const btnIngresar = document.getElementById('btnIngresar')
+    if (email !== '' && contra !== '') {
+        window.location.href = 'inicio.html'
+    }else {
+        alert('Ingresa los datos solicitados')
+    }
+}
+btnIngresar.addEventListener("click", () => {
+    if (email.value) localStorage.setItem("username", email.value);
+}) 
+
+
+
+
+
+
+
+
---
+++ js/init.js
@@ -0,0 +1,44 @@
+const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
+const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
+const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
+const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
+const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
+const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+const EXT_TYPE = ".json"
+const LIST_URL = PRODUCTS_URL + localStorage.getItem("catID") + EXT_TYPE;
+const LIST_INFO_URL = PRODUCT_INFO_URL + localStorage.getItem("prodID") + EXT_TYPE;
+const COMMENTS_URL = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("prodID") + EXT_TYPE;
+
+let showSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
+
+let hideSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+let getJSONData = function(url){
+    let result = {};
+    showSpinner();
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner();
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner();
+        return result;
+    });
+}
