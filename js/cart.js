bf33d59304448316301eeb5b938f4d7b0f9f49d3
tanahernandez28 <110550156+tanahernandez28@users.noreply.github.com>
2022-11-13 14:21:17 -0300

    carrito

--- js/cart.js
+++ js/cart.js
@@ -1,24 +1,3 @@
-let prodAmount = document.getElementById("amount");
-
-document.addEventListener("DOMContentLoaded", function () {
-    getJSONData(CART).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            productOfCart = resultObj.data.articles;
-            showCartProducts(productOfCart);
-        }
-
-        let prodCount = document.getElementsById("amount");
-        for (let i = 0; i < prodCount.length; i++) {
-            prodCount[i].addEventListener("input", function () {
-                subtotalCost();
-                
-            });
-        
-        }
-    })
-})
-
-
 function showCartProducts(cartProducts) {
     let htmlContentToAppend = "";
 
@@ -31,53 +10,66 @@ function showCartProducts(cartProducts) {
                 <td> <img src=`+ product.image + ` style="width: 8.8rem;"> </td>
                 <td> `+ product.name + ` </td>
                 <td> `+ product.currency + " " + product.unitCost + ` </td>
-                <td> <input id="amount" value= `+ product.count +` type="number" min="1" style="width:80px"> </td>
-                <th> `+ product.currency + " " + + ` </th>
+                <td> <input id="amount" value="`+ product.count + `"  type="number" min="1" style="width:80px"> </td>
+                <th id="subTotal"> `+ product.currency + " " + product.unitCost + ` </th>
             </tr>
                    `
-        document.getElementById("cart-list-products").innerHTML = htmlContentToAppend;
-        subtotalCost();
     }
+    document.getElementById("cart-list-products").innerHTML = htmlContentToAppend;
+    subTotalCost();
 }
 
-    function subtotalCost() {
-        let total = 0;
-        let subTotal = 0;
-        let subTotalCost = document.getElementsByClassName("subTotalCost"); 
-        let amount = document.getElementsByClassName("amount");
-        let shippingCost = document.getElementById("shippingCost").value;
-       
-        for (let i = 0; i < cartProducts.length; i++) {
-            let result; // Resultado del subtotal
-            let result2; // Resultado del total final 
+function subTotalCost() {
+    let finalTotal = 0;
+    let almostTotal = 0;
+    let subTotal = document.getElementById("subTotal"); //resultado del subtotal 
+    let count = document.getElementById("amount"); // input cantidad de productos 
+    let shippingCost = document.getElementById("shippingCost").value; //costo de envío
 
-            let cost = cartProducts[i].unitCost;// Variable que contiene el costo unitario de los productos.
-            cartProducts[i].count = count[i].value;// Variable que contiene el value del input de la cantidad de productos
+    for (let i = 0; i < productOfCart.length; i++) {
+        let resultSubtotal; // resultado del subtotal
+        let resultTotal; //resultado del total
 
-            if (cartProducts[i].currency == 'UYU') {
-                result = cost * cartProducts[i].count / 42;
-            } else {
-                result = cost * cartProducts[i].count;
-            }
+        let cost = productOfCart[i].unitCost; // costo unitario por producto 
+        productOfCart[i].count = count[i].value; // value del input, cantidad de productos
 
-            subTotalCost[i].innerHTML = "USD" + result; // Agrega el subtotal al html
-            subTotal += result; 
-
-            if (shippingCost == 1) {
-                result2 = result + (result * 0.05); // Standard
-            } else if (shippingCost == 2) {
-                result2 = result + (result * 0.07); // Express
-            } else if (shippingCost == 3) {
-                result2 = result + (result * 0.15); // Premium
-            } else {
-                result2 = result;
-            }
-    
-            total += result2;
+        if (productOfCart[i].currency == 'UYU') {
+            resultSubtotal = cost * (productOfCart[i].count / 40);
+        } else {
+            resultSubtotal = cost * productOfCart[i].count;
         }
-        document.getElementById("subTotal").innerHTML = subTotal;// Agrega el total al html.
-        document.getElementById("total").innerHTML = total;
-        
+
+        subTotal[i].innerHTML = "USD" + resultSubtotal;
+        almostTotal += resultSubtotal;
+
+        //calcular porcentaje del precio de envío 
+        if (shippingCost == 1) {
+            resultTotal = resultSubtotal + (resultSubtotal * 0.15); //premium 
+        } else if (shippingCost == 2) {
+            resultTotal = resultSubtotal + (resultSubtotal * 0.07); //express
+        } else if (shippingCost == 3) {
+            resultTotal = resultSubtotal + (resultSubtotal * 0.05); //standard
+        }
+        finalTotal += resultTotal;
     }
-    
-    
+    document.getElementById("subTotalCost").innerHTML = almostTotal; //agrego el subtotal 
+    document.getElementById("total").innerHTML = finalTotal; // agrego el total
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productOfCart = resultObj.data.articles;
+
+            showCartProducts(productOfCart);
+        }
+        let amount = document.getElementById("amount");
+
+        //con el evento input calcular el subtotal cuando se cambia la cantidad 
+        for (let i = 0; i < amount.length; i++) {
+            amount[i].addEventListener("input", function () {
+                subTotalCost();
+            })
+        }
+    })
+})
